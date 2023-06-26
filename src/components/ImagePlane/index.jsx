dcfd4ef391523053e40842be5d6964df05475036
MartinGK <mrtgeka@gmail.com>
2023-06-26 19:20:45 -0300

    portrait added

--- src/components/ImagePlane/index.jsx
+++ src/components/ImagePlane/index.jsx
@@ -10,35 +10,13 @@ function ImagePlane({ url, mousePosition, ...props }) {
   const router = useRouter();
   const texture = useLoader(TextureLoader, url);
   const [dataBeforeClicked, setDataBeforeClicked] = useState(null);
-  // Ref para acceder a la malla
   const meshRef = useRef();
 
   useFrame(() => {
     if (meshRef.current) {
-      const [mouseX, mouseY] = mousePosition;
       meshRef.current.rotation.z = 0;
-
-      // console.log(mousePosition)
-      // Configura la rotación en función de la posición del mouse
-      // meshRef.current.rotation.x = (mouseY / window.innerHeight) * Math.PI * 0.25;
-      // meshRef.current.rotation.y = (mouseX / window.innerWidth) * Math.PI * 0.25;
       meshRef.current.rotation.x = props.rotation.x;
       meshRef.current.rotation.y = props.rotation.y;
-
-      // meshRef.current.rotation.x =
-      //   (mouseY /
-      //     (mouseY > window.innerHeight / 2
-      //       ? window.innerHeight
-      //       : window.innerHeight / 2)) *
-      //   Math.PI *
-      //   0.25;
-      // meshRef.current.rotation.y =
-      //   (mouseX / window.innerWidth) * Math.PI * 0.25;
-
-      // Ajusta la opacidad basándose en la posición Z del mesh
-      const zPosition = meshRef.current.position.z;
-      const opacity = 2 / Math.abs(zPosition); // Opacidad inversamente proporcional a la distancia a Z=0
-      meshRef.current.material.opacity = 1;
     }
   });
 
